63492edfea9d494b06ddd1238527e5468eebfb6f
ashhathrh <115910462+ashhathrh@users.noreply.github.com>
2022-10-22 16:43:43 +0800

    Update mymall.py

--- mymall.py
+++ mymall.py
@@ -46,7 +46,8 @@ model.fit(Xtrain, ytrain)
 y_model = model.predict(Xtest)
 
 from sklearn.metrics import accuracy_score
-accuracy_score(ytest, y_model)
+a=accuracy_score(ytest, y_model)
+st.write(a)
 
 from sklearn.metrics import classification_report
 print(classification_report(ytest, y_model))
@@ -59,7 +60,7 @@ from sklearn import metrics
 import numpy as np
 confusion_matrix = metrics.confusion_matrix(ytest, y_model)
 
-print(confusion_matrix)
+st.write(confusion_matrix)
 
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix,display_labels=np.unique(y_mc))
 
