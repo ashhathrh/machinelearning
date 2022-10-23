d85e0c0d0a90721286306e878e715a921308f40a
ashhathrh <115910462+ashhathrh@users.noreply.github.com>
2022-10-23 13:20:57 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -1,23 +1,29 @@
-import seaborn as sns
-import streamlit as st
 import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 
-from sklearn.svm import SVC
+# Reading the Iris.csv file
+data = load_iris()
+
+# Extracting Attributes / Features
+X = data.data
+
+# Extracting Target / Class Labels
+y = data.target
+
+# Import Library for splitting data
 from sklearn.model_selection import train_test_split
-iris = sns.load_dataset('iris') # returns a pandas dataframe
 
-xiris = iris.drop('species', axis=1)  
-yiris = iris['species']
+# Creating Train and Test datasets
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state = 50, test_size = 0.25)
 
-xtrain, xtest, ytrain, ytest = train_test_split(xiris, yiris,random_state=1)
+# Creating Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(X_train,y_train)
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(xtrain, ytrain)
-st.write(clf)
-
-
-
-
-tree.plot_tree(clf.fit(xtrain, ytrain) )
-
-clf.score(xtest, ytest)
+# Predict Accuracy Score
+y_pred = clf.predict(X_test)
+st.write("Train data accuracy:",accuracy_score(y_true = y_train, y_pred=clf.predict(X_train)))
+st.write("Test data accuracy:",accuracy_score(y_true = y_test, y_pred=y_pred))
