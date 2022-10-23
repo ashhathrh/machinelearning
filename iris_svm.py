7ee4d8fafea771bde0851a398ec8295a9de36852
ashhathrh <115910462+ashhathrh@users.noreply.github.com>
2022-10-23 11:26:00 +0800

    Update iris_svm.py

--- iris_svm.py
+++ iris_svm.py
@@ -13,10 +13,10 @@ yiris = iris['species']
 xtrain, xtest, ytrain,ytest = train_test_split(xiris, yiris, random_state = 0)
 
 clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
-st.write('Iris dataset')
-st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
+print('Iris dataset')
+print('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(xtrain, ytrain)))
-st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
+print('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(xtest, ytest)))
 
 # Confusion Matrix
