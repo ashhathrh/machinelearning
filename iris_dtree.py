cc739386582c681d7daf972759737f5bbe0c2c02
ashhathrh <115910462+ashhathrh@users.noreply.github.com>
2022-10-23 13:56:03 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -11,16 +11,19 @@ iris = sns.load_dataset('iris')
 xiris = iris.drop('species', axis=1)  
 yiris = iris['species']
 
-xtrain, xtest, ytrain, ytest = train_test_split(xiris, yiris,random_state=0)
+xtrain, xtest, ytrain, ytest = train_test_split(xiris, yiris,random_state=1)
 
 from sklearn.tree import DecisionTreeClassifier
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(xtrain, ytrain)
 
 
-tree.plot_tree(clf.fit(xtrain, ytrain) )
+
 
 clf.score(xtest, ytest)
+
 fig = plt.figure(figsize=(10, 4))
-sns.boxplot(data=iris)
+clf.fit(xtrain, ytrain)
+tree.plot_tree(clf.fit(xtrain, ytrain))
 st.pyplot(fig)
+clf.score(xtest, ytest)
