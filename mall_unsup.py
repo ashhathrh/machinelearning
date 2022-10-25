a870e1b694dc0cec4fa257eff14cedc73f3f5e2f
ashhathrh <115910462+ashhathrh@users.noreply.github.com>
2022-10-25 13:59:57 +0800

    Update mall_unsup.py

--- mall_unsup.py
+++ mall_unsup.py
@@ -23,11 +23,11 @@ kmeans = KMeans(n_clusters=4)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
 
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
 
 centers = kmeans.cluster_centers_
 centers
 
 fig=plt.figure(figsize=(10,4))
+plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 st.pyplot(fig)
